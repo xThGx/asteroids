064a4d5ea4f171f604c643ab5eb79108a6fc78c6
xThGx <thommy.gangl@gmail.com>
2026-05-04 21:05:25 +0200

    added logger.py, installed jq, added json1 to .gitignore and adjusted main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    b=0
+    while b==0:
+        log_state()
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    screen.fill("black")
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()  
