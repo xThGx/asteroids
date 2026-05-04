653e23da202f3f29ed43df8c019678c30ff935a8
xThGx <thommy.gangl@gmail.com>
2026-05-04 21:20:42 +0200

    added pygame.time.clock() and variable dt = pygamie.time.clock()tick(60) to main.py

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
     b=0
     while b==0:
         log_state()
-    for event in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
-
+        screen.fill("black")
+        pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000  # Limit to 60 FPS and get delta time in seconds
+        #print(f"Delta time: {dt:.4f} seconds")
 if __name__ == "__main__":
     main()  
